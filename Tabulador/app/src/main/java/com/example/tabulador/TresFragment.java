e45102c58aa09e57be4327dc5764eeb3519579c6
Edith Gómez <edith.gomez4@outlook.com>
2019-10-07 00:04:08 -0600

    Actualización Tabulador

--- Tabulador/app/src/main/java/com/example/tabulador/TresFragment.java
+++ Tabulador/app/src/main/java/com/example/tabulador/TresFragment.java
@@ -2,21 +2,20 @@ package com.example.tabulador;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Uno extends Fragment {
+public class TresFragment extends Fragment {
 
 
-    public Uno() {
+    public TresFragment() {
         // Required empty public constructor
     }
 
@@ -25,7 +24,7 @@ public class Uno extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_uno, container, false);
+        return inflater.inflate(R.layout.fragment_tres, container, false);
     }
 
 }
--- Tabulador/app/src/main/java/com/example/tabulador/UnoFragment.java
+++ Tabulador/app/src/main/java/com/example/tabulador/UnoFragment.java
@@ -2,21 +2,20 @@ package com.example.tabulador;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Tres extends Fragment {
+public class UnoFragment extends Fragment {
 
 
-    public Tres() {
+    public UnoFragment() {
         // Required empty public constructor
     }
 
@@ -25,7 +24,7 @@ public class Tres extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tres, container, false);
+        return inflater.inflate(R.layout.fragment_uno, container, false);
     }
 
 }
